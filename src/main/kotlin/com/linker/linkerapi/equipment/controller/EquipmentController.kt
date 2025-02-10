070242c46bf0528a2680e121c09f07a8ab3b10b5
Seongwook Jin <88529990+seongwoooook@users.noreply.github.com>
2025-02-10 22:44:56 +0900

    feat : 대여 가능한 기자재 조회 API 추가 (EquipmentController, EquipmentService 수정)

--- src/main/kotlin/com/linker/linkerapi/equipment/controller/EquipmentController.kt
+++ src/main/kotlin/com/linker/linkerapi/equipment/controller/EquipmentController.kt
@@ -1,7 +1,20 @@
 package com.linker.linkerapi.equipment.controller
 
+import com.linker.linkerapi.equipment.entity.Equipment
+import com.linker.linkerapi.equipment.service.EquipmentService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class EquipmentController {
+@RequestMapping("/api/equipments")
+class EquipmentController(
+    private val equipmentService: EquipmentService
+) {
+    @GetMapping("/available")
+    fun getAvailableEquipments(): ResponseEntity<List<Equipment>> {
+        val availableEquipments: List<Equipment> = equipmentService.getAvailableEquipment()
+        return ResponseEntity.ok(availableEquipments)
+    }
 }
--- src/main/kotlin/com/linker/linkerapi/equipment/service/EquipmentService.kt
+++ src/main/kotlin/com/linker/linkerapi/equipment/service/EquipmentService.kt
@@ -1,7 +1,13 @@
 package com.linker.linkerapi.equipment.service
 
+import com.linker.linkerapi.equipment.entity.Equipment
+import com.linker.linkerapi.equipment.repository.EquipmentRepository
 import org.springframework.stereotype.Service
 
 @Service
-class EquipmentService {
+class EquipmentService(
+    private val equipmentRepository: EquipmentRepository) {
+        fun getAvailableEquipment(): List<Equipment> {
+            return equipmentRepository.findAll().filter { it.availableStock>0 }
+        }
 }
