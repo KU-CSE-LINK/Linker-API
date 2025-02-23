75c998146acb8354572fe9522eeb75467e5fbb75
Seongwook Jin <88529990+seongwoooook@users.noreply.github.com>
2025-02-24 01:47:05 +0900

    주석 제거

--- src/main/kotlin/com/linker/linkerapi/rental/enums/RentalStatus.kt
+++ src/main/kotlin/com/linker/linkerapi/rental/enums/RentalStatus.kt
@@ -1,8 +1,8 @@
 package com.linker.linkerapi.rental.enums
 
 enum class RentalStatus {
-    REQUESTED,       // 대여 신청
-    PREPARED,       // 대여 준비 완료
-    RENTED,          // 대여중
-    RETURNED         // 반납
+    REQUESTED,       
+    PREPARED,       
+    RENTED,          
+    RETURNED         
 }
