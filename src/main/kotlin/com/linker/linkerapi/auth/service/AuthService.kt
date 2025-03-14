6e67d1daf318013bcc34682e589550441eb5cba9
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-03-14 17:48:12 +0900

    Update AuthService.kt

--- src/main/kotlin/com/linker/linkerapi/auth/service/AuthService.kt
+++ src/main/kotlin/com/linker/linkerapi/auth/service/AuthService.kt
@@ -1,10 +1,26 @@
 package com.linker.linkerapi.auth.service
 
+import com.linker.linkerapi.auth.entity.Auth
+import com.linker.linkerapi.auth.exception.LoginFailedException
+import com.linker.linkerapi.auth.repository.AuthRepository
+import com.linker.linkerapi.notification.service.DiscordNotificationService
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class AuthService {
-    fun login() {
-        println("login")
+class AuthService(
+    private val authRepository: AuthRepository,
+    private val discordNotificationService: DiscordNotificationService
+) {
+    fun login(code: String) {
+        authRepository.findByCode(code) ?: throw LoginFailedException()
+    }
+
+    fun sendLoginUrl() {
+        authRepository.deleteAll()
+
+        val code = UUID.randomUUID().toString()
+        val auth = authRepository.save(Auth(code = code))
+        discordNotificationService.sendLoginUrlNotification(auth.code)
     }
 }
