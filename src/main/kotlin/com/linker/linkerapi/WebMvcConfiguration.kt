6e7c274bb573b2c6a666569805d8a7e037213026
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-03-26 16:00:19 +0900

    Add register resolver

--- src/main/kotlin/com/linker/linkerapi/WebMvcConfiguration.kt
+++ src/main/kotlin/com/linker/linkerapi/WebMvcConfiguration.kt
@@ -1,14 +1,17 @@
 package com.linker.linkerapi
 
+import com.linker.linkerapi.common.resolver.AdminAuthenticationResolver
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebMvcConfiguration(
     @Value("\${linker.allowed-origins}")
-    val allowedOrigin: String
+    val allowedOrigin: String,
+    private val adminAuthenticationResolver: AdminAuthenticationResolver,
 ) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry
@@ -17,5 +20,10 @@ class WebMvcConfiguration(
             .allowedOrigins(allowedOrigin)
             .allowedMethods("*")
             .allowCredentials(true)
+    }
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        super.addArgumentResolvers(resolvers)
+        resolvers.add(adminAuthenticationResolver)
     }
 }
