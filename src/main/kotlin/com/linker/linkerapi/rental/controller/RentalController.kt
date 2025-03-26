a166cc343f56d239b8a48b1b662e94f596f33a34
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-03-26 20:42:08 +0900

    Remove not blank validation

--- src/main/kotlin/com/linker/linkerapi/rental/controller/RentalController.kt
+++ src/main/kotlin/com/linker/linkerapi/rental/controller/RentalController.kt
@@ -5,7 +5,6 @@ import com.linker.linkerapi.rental.entity.Rental
 import com.linker.linkerapi.rental.service.RentalService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import jakarta.validation.constraints.NotBlank
 import org.springframework.web.bind.annotation.*
 
 @Tag(name = "Rental Controller")
@@ -28,9 +27,7 @@ class RentalController(
     @Operation(summary = "본인 신청 내역 조회", description = "학번, 전화번호, 이름을 모두 입력하여 대여 신청 내역을 조회합니다.")
     @GetMapping("/rentals")
     fun getMyRentals(
-        @NotBlank
         @RequestParam("name") name: String,
-        @NotBlank
         @RequestParam("studentId") studentId: Long,
     ): List<Rental> {
         return rentalService.getRentalsByUserInfo(name, studentId)
