4b4595e298bbc154c976816a3152e827f00203cc
Seongwook Jin <88529990+seongwoooook@users.noreply.github.com>
2025-04-06 17:56:55 +0900

    feat: 대여 반납일 자동 문자 알림 기능 구현

--- src/main/kotlin/com/linker/linkerapi/rental/repository/RentalRepository.kt
+++ src/main/kotlin/com/linker/linkerapi/rental/repository/RentalRepository.kt
@@ -1,11 +1,19 @@
 package com.linker.linkerapi.rental.repository
 
 import com.linker.linkerapi.rental.entity.Rental
+import com.linker.linkerapi.rental.enums.RentalStatus
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDateTime
 
 interface RentalRepository : JpaRepository<Rental, Long> {
     fun findAllByStudentIdAndName(
         studentId: Long,
         name: String
+    ): List<Rental>
+
+    fun findAllByReturnDateBetweenAndStatus(
+        startOfDay: LocalDateTime,
+        endOfDay: LocalDateTime,
+        status: RentalStatus
     ): List<Rental>
 }
