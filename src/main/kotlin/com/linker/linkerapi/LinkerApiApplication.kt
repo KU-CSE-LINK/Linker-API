3eac665fe96347a72df8777b9f57ce1579c7694f
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-04-08 11:40:42 +0900

    Add timezone

--- src/main/kotlin/com/linker/linkerapi/LinkerApiApplication.kt
+++ src/main/kotlin/com/linker/linkerapi/LinkerApiApplication.kt
@@ -1,14 +1,21 @@
 package com.linker.linkerapi
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.*
 
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
-class LinkerApiApplication
+class LinkerApiApplication {
+    @PostConstruct
+    fun started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<LinkerApiApplication>(*args)
